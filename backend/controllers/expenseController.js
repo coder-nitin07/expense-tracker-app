06dc3d1c3e274172f462fad2d3e865d2e2681a6d
coder-nitin07 <nitinthakur87504@gmail.com>
2025-07-21 17:58:10 +0530

    Create Updaet and Delete API

--- backend/controllers/expenseController.js
+++ backend/controllers/expenseController.js
@@ -60,4 +60,47 @@ const getExpenseById = async (req, res)=>{
     }  
 };
 
-module.exports = { createExpense, getAllExpenseOfEmployee, getExpenseById };
+// Update Expense
+const updateExpense = async (req, res)=>{
+    try {
+        const { id } = req.params;
+
+        const getExpense = await Expense.findById(id);
+        if(!getExpense){
+            return res.status(404).json({ message: 'Expense Data not found' });
+        }
+
+        const { title, amount, category, description } = req.body;
+
+        if(title !== undefined) getExpense.title = title;
+        if(amount !== undefined) getExpense.amount = amount;
+        if(category !== undefined) getExpense.category = category;
+        if(description !== undefined) getExpense.description = description;
+
+        const updatedExpense = await getExpense.save();
+        res.status(200).json({ message: 'Expense Updated Successfully', updatedExpense });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ message: 'Something went wrong' });
+    }
+};
+
+// Delete Expense
+const deleteExpense = async (req, res)=>{
+    try {
+        const { id } = req.params;
+        const expenseId = await Expense.findById(id);
+
+        if(!expenseId){
+            return res.status(404).json({ message: 'Expense not found' });
+        }
+
+        const deletedExpense = await Expense.findByIdAndDelete(id);
+        res.status(200).json({ message: 'Expense Deleted Succesfully', deletedExpense });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ message: 'Something went wrong' });
+    }
+};
+
+module.exports = { createExpense, getAllExpenseOfEmployee, getExpenseById, updateExpense, deleteExpense };
--- backend/routes/expenseRouter.js
+++ backend/routes/expenseRouter.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { createExpense, getAllExpenseOfEmployee, getExpenseById } = require('../controllers/expenseController');
+const { createExpense, getAllExpenseOfEmployee, getExpenseById, updateExpense, deleteExpense } = require('../controllers/expenseController');
 const { validateExpense } = require('../middleware/validateExpense');
 const authMiddleware = require('../middleware/authMiddleware');
 const checkAdminOrOwnerOfExpense = require('../middleware/roleMiddleware');
@@ -8,5 +8,7 @@ const expenseRouter = express.Router();
 expenseRouter.post('/createExpese', authMiddleware, validateExpense, createExpense);
 expenseRouter.get('/getExpenseOfEmployee', authMiddleware, getAllExpenseOfEmployee);
 expenseRouter.get('/getExpenseById/:id', authMiddleware, checkAdminOrOwnerOfExpense, getExpenseById);
+expenseRouter.put('/updateExpense/:id', authMiddleware, checkAdminOrOwnerOfExpense, validateExpense, updateExpense);
+expenseRouter.delete('/deleteExpense/:id', authMiddleware, checkAdminOrOwnerOfExpense, deleteExpense);
 
 module.exports = { expenseRouter };
